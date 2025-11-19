47fa88878c29da501a326880c7dad0cc41b709fe
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-11-19 19:34:23 +0530

    lecture 1 and 2

---
+++ src/main/java/com/example/WalletAppReal/services/saga/steps/CreditDestinationWalletStep.java
@@ -0,0 +1,72 @@
+package com.example.WalletAppReal.services.saga.steps;
+
+import com.example.WalletAppReal.models.Wallet;
+import com.example.WalletAppReal.repostiory.WalletRepository;
+import com.example.WalletAppReal.services.saga.ISagaStep;
+import com.example.WalletAppReal.services.saga.SagaContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CreditDestinationWalletStep implements ISagaStep {
+
+
+    private final WalletRepository walletRepository;
+    @Override
+    @Transactional
+    public boolean execute(SagaContext sagaContext) {
+        // Step 1: Get the destination wallet id from the context
+        Long toWalletId = sagaContext.getLong("toWalletId");
+        BigDecimal amount = sagaContext.getBigDecimal("amount");
+        log.info("Crediting destination wallet {} with amount {}", toWalletId, amount);
+
+        // Step 2: Fetch the destination wallet from the database with a lock
+        Wallet wallet = walletRepository.findByIdWithLock(toWalletId)
+                .orElseThrow(() -> new RuntimeException("Wallet not found with id: " + toWalletId));
+        log.info("Wallet fetched with balance {}", wallet.getBalance());
+        sagaContext.put("OriginalToWalletBalance", wallet.getBalance());
+
+        // Step 3: Credit the destination wallet
+        wallet.credit(amount);
+        walletRepository.save(wallet);
+        log.info("Wallet credited with balance {}", wallet.getBalance());
+        log.info("Wallet saved in the db with balance {}", wallet.getBalance());
+        sagaContext.put("toWalletBalanceAfterCredit",wallet.getBalance());
+        log.info("Credit Destination wallet step executed successfully");
+        return true;
+    }
+
+    @Override
+    public boolean compensate(SagaContext sagaContext) {
+        // Step 1: Get the destination wallet id from the context
+        Long toWalletId = sagaContext.getLong("toWalletId");
+        BigDecimal amount = sagaContext.getBigDecimal("amount");
+        log.info("Compensating credit of destination wallet {} with amount {}", toWalletId, amount);
+
+        // Step 2: Fetch the destination wallet from the database with a lock
+        Wallet wallet = walletRepository.findByIdWithLock(toWalletId)
+                        .orElseThrow(() -> new RuntimeException("Wallet not found with id: " + toWalletId));
+        sagaContext.put("toWalletBalanceBeforeCreditCompensation",wallet.getBalance());
+        log.info("Wallet fetched with balance {}", wallet.getBalance());
+
+        // Step 3: Debit the destination wallet
+        wallet.debit(amount);
+        walletRepository.save(wallet);
+        log.info("Wallet saved with balance {}", wallet.getBalance());
+        sagaContext.put("toWalletBalanceAfterCreditCompensation",wallet.getBalance());
+
+        log.info("Credit compensation of destination wallet step executed successfully");
+        return true;
+    }
+
+    @Override
+    public String getStepName() {
+        return "CreditDestinationWalletStep";
+    }
+}
---
+++ src/main/java/com/example/WalletAppReal/services/saga/steps/DebitSourceWalletStep.java
@@ -0,0 +1,61 @@
+package com.example.WalletAppReal.services.saga.steps;
+
+import com.example.WalletAppReal.models.Wallet;
+import com.example.WalletAppReal.repostiory.WalletRepository;
+import com.example.WalletAppReal.services.saga.ISagaStep;
+import com.example.WalletAppReal.services.saga.SagaContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DebitSourceWalletStep implements ISagaStep {
+
+    private final WalletRepository walletRepository;
+
+    @Override
+    public boolean execute(SagaContext sagaContext) {
+        Long fromWalletId = sagaContext.getLong("fromWalletId");
+        BigDecimal amount = sagaContext.getBigDecimal("amount");
+        log.info("Debiting source wallet {} with amount {}", fromWalletId, amount);
+        Wallet wallet = walletRepository.findByIdWithLock(fromWalletId)
+                .orElseThrow(() -> new RuntimeException("Wallet not found"));
+        log.info("Wallet Fetched with balance {}", wallet.getBalance());
+        sagaContext.put("originalSourceWalletBalance",wallet.getBalance());
+        wallet.debit(amount);
+        walletRepository.save(wallet);
+        log.info("Wallet saved with balance {}", wallet.getBalance());
+        sagaContext.put("sourceWalletBalanceAfterDebit",wallet.getBalance());
+        log.info("Debit source Wallet step executed successfully");
+        return true;
+    }
+
+    @Override
+    public boolean compensate(SagaContext sagaContext) {
+        Long fromWalletId = sagaContext.getLong("fromWalletId");
+        BigDecimal amount = sagaContext.getBigDecimal("amount");
+        log.info("Compensating source wallet {} with amount {}", fromWalletId, amount);
+
+        Wallet wallet = walletRepository.findByIdWithLock(fromWalletId)
+                .orElseThrow(() -> new RuntimeException("Wallet not found"));
+
+        log.info("Wallet Fetched with balance {}", wallet.getBalance());
+        sagaContext.put("sourceWalletBalanceBeforeCreditCompensation",wallet.getBalance());
+
+        wallet.credit(amount);
+        walletRepository.save(wallet);
+        log.info("Wallet saved with balance {}", wallet.getBalance());
+        sagaContext.put("sourceWalletBalanceAfterCreditCompensation",wallet.getBalance());
+        log.info("Credit compensation of source wallet step executed successfully");
+        return true;
+    }
+
+    @Override
+    public String getStepName() {
+        return "DebitSourceWalletStep";
+    }
+}
